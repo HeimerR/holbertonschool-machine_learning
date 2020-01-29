23ae3fa5aedea9e5943d846198c513a19ff499cc
heimerr <732@holbertonschool.com>
2020-01-29 01:00:52 +0000

    use of slice function

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Slice Like A Ninja """
+import numpy as np
+
+
+def np_slice(matrix, axes={}):
+    """ slices a matrix along a specific axes """
+    slice_all = slice(None, None, None)
+    shape_matrix = matrix.shape
+    slice_list = [slice_all] * len(shape_matrix)
+    for key, value in sorted(axes.items()):
+        slice_object = slice(*value)
+        slice_list[key] = slice_object
+    matrix = matrix[tuple(slice_list)]
+    return matrix
