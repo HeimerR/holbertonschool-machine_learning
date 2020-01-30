ad0235667fef3df2ec91880f46b99d478e212a09
heimerr <732@holbertonschool.com>
2020-01-30 18:26:26 +0000

    firt(1-4) plot tasks added

--- math/0x00-linear_algebra/102-main.py
+++ math/0x00-linear_algebra/102-main.py
@@ -4,20 +4,24 @@ import numpy as np
 import time
 cat_matrices = __import__('102-squashed_like_sardines').cat_matrices
 
-mat1 = [1, 2, 3]
-mat2 = [4, 5, 6]
+mat1 = [[[[[1.1], [2.2], [3.3]]]]]
+mat2 = [[[[[4.2], [5.2], [6.1]]]]]
 np_mat1 = np.array(mat1)
 np_mat2 = np.array(mat2)
 
 t0 = time.time()
-m = cat_matrices(mat1, mat2)
+m = cat_matrices(mat1, mat2, axis=1)
 t1 = time.time()
 print(t1 - t0)
 print(m)
 t0 = time.time()
-np.concatenate((np_mat1, np_mat2))
+m2 = np.concatenate((np_mat1, np_mat2), axis=1)
 t1 = time.time()
 print(t1 - t0, "\n")
+print(m2)
+print(mat1)
+print(mat2)
+print("---------------------------------------------------")
 
 mat1 = [[1, 2], [3, 4]]
 mat2 = [[5, 6], [7, 8]]
---
+++ math/0x01-plotting/4-frequency.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.hist(student_grades, edgecolor='black', bins=range(0, 110, 10))
+plt.suptitle('Project A')
+plt.xlim(0, 100)
+plt.ylim(0, 30)
+plt.show()
