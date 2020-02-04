fe60c4117ea6b345b5786cac0a2416123b1b0deb
heimerr <732@holbertonschool.com>
2020-02-04 13:46:58 +0000

    negative condition deleted

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     """ calculates sum(i=0; n; i**2) """
-    if type(n) != int or n < 0:
+    if type(n) != int:
         return None
     return int((n * (n + 1) * (2 * n + 1)) / 6)
