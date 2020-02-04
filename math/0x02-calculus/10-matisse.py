021a20d9af8848a5d34d291e59ee9895ec9cef12
heimerr <732@holbertonschool.com>
2020-02-04 18:39:34 +0000

    len(poly) == 0 return None

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -6,6 +6,8 @@ def poly_derivative(poly):
     """  calculates the derivative of a polynomial """
     if type(poly) != list:
         return None
+    if len(poly) == 0:
+        return None
     if not all(isinstance(n, (int, float)) for n in poly):
         return None
     dx = [i*poly[i] for i in range(len(poly))]
