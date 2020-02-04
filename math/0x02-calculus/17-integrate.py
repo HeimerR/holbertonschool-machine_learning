754b4421a3b9462c2d443d16f6762df8f3f60ee1
heimerr <732@holbertonschool.com>
2020-02-04 19:06:38 +0000

    trim zeros at the end

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -11,4 +11,9 @@ def poly_integral(poly, C=0):
     if not all(isinstance(n, (int, float)) for n in poly):
         return None
     integral = [float(C)] + [poly[i] / (i + 1) for i in range(len(poly))]
-    return [int(coef) if coef.is_integer() else coef for coef in integral]
+    integ_fit = [int(coef) if coef.is_integer() else coef for coef in integral]
+    i = len(integ_fit) - 1
+    while integ_fit[i] == 0:
+        integ_fit.pop(i)
+        i -= 1
+    return integ_fit
