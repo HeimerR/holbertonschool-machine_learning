dc9a787a871bfac56e6685744c295adba3575599
heimerr <732@holbertonschool.com>
2020-02-04 18:52:40 +0000

    len(poly) == 0 return None

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,6 +6,8 @@ def poly_integral(poly, C=0):
     """  calculates the integral of a polynomial """
     if not isinstance(C, (int, float)) or not isinstance(poly, list):
         return None
+    if len(poly) == 0:
+        return None
     if not all(isinstance(n, (int, float)) for n in poly):
         return None
     integral = [float(C)] + [poly[i] / (i + 1) for i in range(len(poly))]
