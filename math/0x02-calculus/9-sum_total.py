f90975be912c043d13d18a381bf35430c2857445
heimerr <732@holbertonschool.com>
2020-02-04 13:58:37 +0000

    n<=0 return none

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,8 +4,6 @@
 
 def summation_i_squared(n):
     """ calculates sum(i=0; n; i**2) """
-    if type(n) != int:
+    if type(n) != int or n <= 0:
         return None
-    if n <= 0:
-        return 0
     return int((n * (n + 1) * (2 * n + 1)) / 6)
