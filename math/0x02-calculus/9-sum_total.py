5ced5938792c3dbea7b4bbfc91e84fec874196fc
heimerr <732@holbertonschool.com>
2020-02-04 12:48:32 +0000

    recursion instead loop

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,4 +4,8 @@
 
 def summation_i_squared(n):
     """ calculates sum(i=0; n; i**2) """
-    return sum([i**2 for i in range(1, n + 1)])
+    if type(n) != int:
+        return None
+    if n == 1:
+        return 1
+    return n**2 + summation_i_squared((n-1))
