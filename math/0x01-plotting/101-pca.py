5eed3e8cf3c2010491e08296182e5ce9e8c48420
heimerr <732@holbertonschool.com>
2020-02-05 23:15:44 +0000

    3dtask added

--- math/0x01-plotting/101-pca.py
+++ math/0x01-plotting/101-pca.py
@@ -2,14 +2,8 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
-#import pickle as pl
 
-
-#with open("pca.npz", 'rb') as handle:
-#     my_array = pl.load(handle)
-#lib = np.array(my_array)
-
-lib = np.loadtxt("bezdekIris.data", delimiter=",")
+lib = np.load("pca.npz")
 data = lib["data"]
 labels = lib["labels"]
 
@@ -18,5 +12,16 @@ norm_data = data - data_means
 _, _, Vh = np.linalg.svd(norm_data)
 pca_data = np.matmul(norm_data, Vh[:3].T)
 
-print(data_means)
-print(norm_data)
+print(pca_data)
+print(len(pca_data.T[0]))
+print(len(pca_data.T[1]))
+print(len(pca_data.T[2]))
+fig = plt.figure()
+fig.suptitle("PCA of Iris Dataset")
+ax = Axes3D(fig)
+ax.set_xlabel('U1')
+ax.set_ylabel('U2')
+ax.set_zlabel('U3')
+plt.plasma()
+ax.scatter(pca_data.T[0], pca_data.T[1], pca_data.T[2], c=labels)
+plt.show()
