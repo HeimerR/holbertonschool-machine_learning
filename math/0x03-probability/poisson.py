f510bf68e8726d2663128e91d3ffb5c319b9e007
heimerr <732@holbertonschool.com>
2020-02-06 18:38:18 +0000

    fmp function added

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -4,6 +4,9 @@
 
 class Poisson:
     """ prime class """
+
+    e = 2.7182818285
+
     def __init__(self, data=None, lambtha=1.):
         """Initialization of the poisson class"""
         self.lambtha = float(lambtha)
@@ -16,3 +19,14 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """ Calculates the value of the PMF """
+        if type(k) != int:
+            k = int(k)
+        if k <= 0:
+            return 0
+        fac = 1
+        for i in range(1, k + 1):
+            fac *= i
+        return ((self.lambtha ** k) * Poisson.e ** (self.lambtha * (-1))) / fac
