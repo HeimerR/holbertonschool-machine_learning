e1434a792d6c66dc57afc8c88af128d90354d439
heimerr <732@holbertonschool.com>
2020-02-06 17:57:03 +0000

    rollback lamnda location

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -6,10 +6,10 @@ class Poisson:
     """ prime class """
     def __init__(self, data=None, lambtha=1.):
         """Initialization of the poisson class"""
+        self.lambtha = float(lambtha)
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
-            self.lambtha = float(lambtha)
         else:
             if type(data) != list:
                 raise TypeError("data must be a list")
