5c22a8f91764c2d303f2248e37f0cc9a4b8c3741
heimerr <732@holbertonschool.com>
2020-02-06 17:48:31 +0000

    poisson class added

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Initialize Poisson """
+
+
+class Poisson:
+    """ prime class """
+    def __init__(self, data=None, lambtha=1.):
+        """Initialization of the poisson class"""
+        self.lambtha = float(lambtha)
+        if data is None:
+            data = lambtha
+            if lambtha < 0:
+                raise ValueError("lambtha must be a positive value")
+        else:
+            if type(data) != list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
