c7bfcf540e6a871665d681cf309e21882b99b2e5
heimerr <732@holbertonschool.com>
2020-02-16 15:09:35 +0000

    no splited indentation

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -23,11 +23,9 @@ class DeepNeuralNetwork:
         for ly in range(len(layers)):
             self.weights["b" + str(ly + 1)] = np.zeros((layers[ly], 1))
             if ly == 0:
-                self.__weights["W" + str(ly + 1)] = (np.random.randn
-                                                     (layers[ly], nx) *
-                                                     np.sqrt(2/nx))
+                heetal = np.random.randn(layers[ly], nx) * np.sqrt(2/nx)
+                self.weights["W" + str(ly + 1)] = heetal
             else:
-                self.__weights["W" + str(ly + 1)] = (np.random.randn
-                                                     (layers[ly],
-                                                      layers[ly-1]) *
-                                                     np.sqrt(2/layers[ly-1]))
+                factor = np.sqrt(2/layers[ly - 1])
+                heetal = np.random.randn(layers[ly], layers[ly - 1]) * factor
+                self.weights["W" + str(ly + 1)] = heetal
