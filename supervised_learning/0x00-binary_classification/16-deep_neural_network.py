b3e4e94de4c797442024c88f250d8746bfa9535d
heimerr <732@holbertonschool.com>
2020-02-16 03:16:08 +0000

    deep neural net class added

---
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Class DeepNeuralNetwork """
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ class DeepNeuralNetwork """
+    def __init__(self, nx, layers):
+        """ init for DeepNeuralNetwork """
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(layers) != list:
+            raise TypeError('layers must be a list of positive integers')
+        if not all(n > 0 for n in layers):
+            raise TypeError('layers must be a list of positive integers')
+        self.nx = nx
+        self.layers = layers
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+        for l in range(len(layers)):
+            self.weights["b" + str(l + 1)] = np.zeros((layers[l], 1))
+            self.weights["W" + str(l + 1)] = (np.random.randn(layers[l],
+                                              layers[l-1])
+                                              * np.sqrt(2/layers[l-1]))
