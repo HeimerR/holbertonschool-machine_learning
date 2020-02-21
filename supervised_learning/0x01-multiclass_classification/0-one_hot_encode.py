fa9260c7bca6a926699653e29e9d4f15012d252e
heimerr <732@holbertonschool.com>
2020-02-21 12:30:49 +0000

    case classes not an int

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -5,7 +5,9 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """ converts a numeric label vector into a one-hot matrix """
-    if not isinstance(Y, np.ndarray) or len(Y) == 0 or classes <= np.amax(Y):
+    if not isinstance(Y, np.ndarray) or len(Y) == 0:
+        return None
+    if  type(classes) != int or classes <= np.amax(Y):
         return None
     one_hot = np.zeros((classes, len(Y)))
     axis = np.arange(len(Y))
