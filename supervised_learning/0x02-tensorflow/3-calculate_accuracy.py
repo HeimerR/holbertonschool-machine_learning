5e3136bca9e7cff3024aac506881657a0b9fd530
heimerr <732@holbertonschool.com>
2020-02-25 13:38:46 +0000

    equal n rpund solution

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -5,5 +5,6 @@ import tensorflow as tf
 
 def calculate_accuracy(y, y_pred):
     """ calculates the accuracy of a prediction """
-    ac, _ = tf.metrics.accuracy(predictions=y_pred, labels=y)
-    return tf.math.reduce_mean(_)
+    correct_prediction = tf.equal(tf.round(y_pred), tf.round(y))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    return accuracy
