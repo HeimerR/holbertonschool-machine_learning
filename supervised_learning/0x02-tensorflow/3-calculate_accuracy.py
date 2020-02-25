9bb846f814149c66b72dbd28f3e7e7171c9a6e1b
heimerr <732@holbertonschool.com>
2020-02-25 13:32:55 +0000

    accuracy + mean functions, second return

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -6,4 +6,4 @@ import tensorflow as tf
 def calculate_accuracy(y, y_pred):
     """ calculates the accuracy of a prediction """
     ac, _ = tf.metrics.accuracy(predictions=y_pred, labels=y)
-    return tf.math.reduce_mean(ac)
+    return tf.math.reduce_mean(_)
