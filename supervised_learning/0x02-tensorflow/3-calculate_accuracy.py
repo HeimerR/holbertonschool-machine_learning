eec402ec1570e09fb00d33e22955adf28a6a1a76
heimerr <732@holbertonschool.com>
2020-02-25 21:20:27 +0000

    equal version

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 
 def calculate_accuracy(y, y_pred):
     """ calculates the accuracy of a prediction """
-    correct_prediction = tf.equal(tf.round(y_pred), tf.round(y))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    #prediction = tf.argmax(prob, 1)
+    equality = tf.equal(y_pred, y)
+    accuracy = tf.reduce_mean(tf.cast(equality, tf.float32))
+
     return accuracy
