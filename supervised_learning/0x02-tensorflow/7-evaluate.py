c282c3a9b7e04566cf4a36e42816a25a636041fc
heimerr <732@holbertonschool.com>
2020-02-26 03:52:16 +0000

    import model added

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Evaluate """
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """ evaluates the output of a neural network """
+    sess = tf.Session()
+    saver = tf.train.import_meta_graph(save_path + ".meta")
+    saver.restore(sess, tf.train.latest_checkpoint('./'))
+
+    graph = tf.get_default_graph()
+    x = graph.get_tensor_by_name("x:0")
+    y = graph.get_tensor_by_name("y:0")
+    forward_prop = graph.get_tensor_by_name("layer_2/BiasAdd:0")
+    loss = graph.get_tensor_by_name("softmax_cross_entropy_loss/value:0")
+    accuracy = graph.get_tensor_by_name("Mean:0")
+    feed_dict = {x: X, y: Y}
+    fp = sess.run(forward_prop, feed_dict)
+    ac = sess.run(accuracy, feed_dict)
+    ls = sess.run(loss, feed_dict)
+    return fp, ac, ls
