0bf7af7f0fe29f1d0195ecace1d5e2e5a5687ba7
heimerr <732@holbertonschool.com>
2020-02-27 01:42:29 +0000

    saver return used

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -40,5 +40,5 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
                 print("\tValidation Accuracy: {}".format(acc_v))
             if i < iterations:
                 sess.run(train_op,  feed_dict={x: X_train, y: Y_train})
-        saver.save(sess, save_path)
+        save_path = saver.save(sess, save_path)
     return save_path
