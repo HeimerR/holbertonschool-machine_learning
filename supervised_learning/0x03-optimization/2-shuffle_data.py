e2cca52f113e7241d4e6fd32f36650d751fccaa8
heimerr <732@holbertonschool.com>
2020-03-01 22:15:40 +0000

    another seed (0) added

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -5,6 +5,7 @@ import numpy as np
 
 def shuffle_data(X, Y):
     """  shuffles the data points in two matrices the same way """
+    np.random.seed(0)
     X_shuffled = np.random.permutation(X)
     np.random.seed(0)
     Y_shuffled = np.random.permutation(Y)
