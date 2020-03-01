2babcf6074435b842a3662d602ec7b1d180d2b72
heimerr <732@holbertonschool.com>
2020-03-01 22:36:40 +0000

    get_state method rand

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -5,8 +5,8 @@ import numpy as np
 
 def shuffle_data(X, Y):
     """  shuffles the data points in two matrices the same way """
-    np.random.seed(0)
+    st0 = np.random.get_state()
     X_shuffled = np.random.permutation(X)
-    np.random.seed(0)
+    np.random.set_state(st0)
     Y_shuffled = np.random.permutation(Y)
     return X_shuffled, Y_shuffled
