538917647f3ee6a3e84a7486ed7d08236be80626
heimerr <732@holbertonschool.com>
2020-03-03 13:35:22 +0000

    moments version

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -8,5 +8,7 @@ def create_batch_norm_layer(prev, n, activation):
     in tensorflow
     """
     x = tf.layers.Dense(units=n, activation=None)
-    f = tf.layers.batch_normalization(inputs=x(prev))
+    m, s = tf.nn.moments(x(prev), axes=[0])
+    f = (tf.nn.batch_normalization(x(prev), mean=m, variance=s,
+         offset=None, scale=None, variance_epsilon=1e-8))
     return activation(f)
