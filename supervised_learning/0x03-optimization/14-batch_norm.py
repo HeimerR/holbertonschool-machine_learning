404992ad16b3e343cf87c952f4d314429ddb0f4f
heimerr <732@holbertonschool.com>
2020-03-03 13:53:31 +0000

    activation in dense

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -7,8 +7,8 @@ def create_batch_norm_layer(prev, n, activation):
     """  creates a batch normalization layer for a neural network
     in tensorflow
     """
-    x = tf.layers.Dense(units=n, activation=None)
+    x = tf.layers.Dense(units=n, activation=activation)
     m, s = tf.nn.moments(x(prev), axes=[0])
     f = (tf.nn.batch_normalization(x(prev), mean=m, variance=s,
          offset=None, scale=None, variance_epsilon=1e-8))
-    return activation(f)
+    return f
