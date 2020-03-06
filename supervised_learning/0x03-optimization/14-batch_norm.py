30ed4042b93988b2257dc9879a88a64d7a53f24e
heimerr <732@holbertonschool.com>
2020-03-06 23:58:02 +0000

    initialize none

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -7,10 +7,6 @@ def create_batch_norm_layer(prev, n, activation):
     """  creates a batch normalization layer for a neural network
     in tensorflow
     """
-    beta = (tf.get_variable("beta", [n],
-            initializer=tf.zeros_initializer(), trainable=True))
-    gamma = (tf.get_variable("gamma", [n],
-             initializer=tf.ones_initializer(), trainable=True))
 
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     x = tf.layers.Dense(units=n, activation=None, kernel_initializer=init)
@@ -18,6 +14,6 @@ def create_batch_norm_layer(prev, n, activation):
     m, s = tf.nn.moments(x(prev), axes=[0])
 
     f = (tf.nn.batch_normalization(x(prev), mean=m, variance=s,
-         offset=beta, scale=gamma, variance_epsilon=1e-8))
+         offset=None, scale=None, variance_epsilon=1e-8))
 
     return activation(f)
