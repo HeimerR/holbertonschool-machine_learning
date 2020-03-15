2860aafe39f83917f7a396ba365ee0485d6495fa
heimerr <732@holbertonschool.com>
2020-03-15 17:56:27 +0000

    keras version wirh classes

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -9,4 +9,4 @@ def one_hot(labels, classes=None):
     The last dimension of the one-hot matrix must be the number of classes
     Returns: the one-hot matrix
     """
-    return K.utils.to_categorical(labels)
+    return K.utils.to_categorical(labels, classes)
