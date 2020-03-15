512a7e4e9499b37f682294df06561806ee881fe6
heimerr <732@holbertonschool.com>
2020-03-15 17:52:55 +0000

    keras version

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ One Hot """
-import numpy as np
+import tensorflow.keras as K
 
 
 def one_hot(labels, classes=None):
@@ -9,9 +9,4 @@ def one_hot(labels, classes=None):
     The last dimension of the one-hot matrix must be the number of classes
     Returns: the one-hot matrix
     """
-    if classes is None:
-        classes = max(labels) - min(labels) + 1
-    one_hot = np.zeros((len(labels), classes))
-    axis = np.arange(len(labels))
-    one_hot[axis, labels] = 1
-    return one_hot
+    return K.utils.to_categorical(labels)
