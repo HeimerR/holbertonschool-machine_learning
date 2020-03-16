9118ec88082c8380d1c6134a822de490aee7ba2e
heimerr <732@holbertonschool.com>
2020-03-16 23:20:26 +0000

    1 - rate added

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -10,7 +10,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
               kernel_regularizer=K.regularizers.l2(lambtha),
               input_shape=(nx,)))
     for i in range(1, len(layers)):
-        model.add(K.layers.Dropout(keep_prob))
+        model.add(K.layers.Dropout(1 - keep_prob))
         model.add(K.layers.Dense(layers[i], activation=activations[i],
                   kernel_regularizer=K.regularizers.l2(lambtha)))
     return model
