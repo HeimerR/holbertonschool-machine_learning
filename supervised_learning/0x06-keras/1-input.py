d47984548ee9e79c53fb46399e8d264e6b4147e2
heimerr <732@holbertonschool.com>
2020-03-17 00:04:47 +0000

    models.Model instead .Model

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -13,4 +13,4 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         x = K.layers.Dropout(1 - keep_prob)(x)
         x = K.layers.Dense(layers[i], activation=activations[i],
                            kernel_regularizer=K.regularizers.l2(lambtha))(x)
-    return K.Model(inputs=inputs, outputs=x)
+    return K.models.Model(inputs=inputs, outputs=x)
