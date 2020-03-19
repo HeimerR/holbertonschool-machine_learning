32e75ec60bf4ffde1df337b7fc9dc28c9304df49
heimerr <732@holbertonschool.com>
2020-03-19 00:19:30 +0000

    run but not the correct image

--- math/0x04-convolutions_and_pooling/4-convolve_channels.py
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -53,16 +53,12 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     out_h = int(((new_h-kh)/sh) + 1)
     out_w = int(((new_w-kw)/sw) + 1)
     conv = np.zeros((m, out_h, out_w))
-    img2 = np.arange(m)
-    channel = np.arange(c)
-    img, _1, _2, ch = np.indices(new_images.shape)
-    print(img[0])
-    #print(new_images[img, 0:32, 0:32, img])
+    img = np.arange(m)
+    ch = np.arange(c)
     for j in range(out_h):
         for i in range(out_w):
-            conv[img2, j, i] = (np.sum(new_images[img,
+            conv[img, j, i] = (np.sum(new_images[img[:, np.newaxis],
                                j*sh:(kh+(j*sh)),
                                i*sw:(kw+(i*sw)), ch] *
                                kernel, axis=(1, 2, 3)))
-
     return conv
