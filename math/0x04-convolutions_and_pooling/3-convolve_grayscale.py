942400308458987b1f92752bb6e548c09f893470
heimerr <732@holbertonschool.com>
2020-03-20 02:17:53 +0000

    same fixed

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -33,32 +33,28 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     kw = kernel.shape[1]
     sh = stride[0]
     sw = stride[1]
-    if padding == 'valid':
-        new_h = h
-        new_w = w
-        new_images = images
     if padding == 'same':
-        ph = int((kh - 1)/2)
-        pw = int((kw - 1)/2)
+        ph = int(((h-1)*sh+kh-h)/2)
+        pw = int(((w-1)*sw+kw-w)/2)
         if kh % 2 == 0:
-            ph = int(kh/2)
+            ph = int((h*sh+kh-h)/2)
         if kw % 2 == 0:
-            pw = int(kw/2)
+            pw = int((w*sw+kw-w)/2)
     if type(padding) == 'tuple':
         ph = padding[0]
         pw = padding[1]
     if padding == 'same' or type(padding) == 'tuple':
-        new_images = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
-                            mode='constant', constant_values=0)
-        new_h = new_images.shape[1]
-        new_w = new_images.shape[2]
-    out_h = int(((new_h-kh)/sh) + 1)
-    out_w = int(((new_w-kw)/sw) + 1)
-    conv = np.zeros((m, out_h, out_w))
+        images = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
+                        mode='constant', constant_values=0)
+        h = images.shape[1]
+        w = images.shape[2]
+    conv_h = int(((h-kh)/sh) + 1)
+    conv_w = int(((w-kw)/sw) + 1)
+    conv = np.zeros((m, conv_h, conv_w))
     img = np.arange(m)
-    for j in range(0, out_h):
-        for i in range(0, out_w):
-            conv[img, j, i] = (np.sum(new_images[img,
+    for j in range(conv_h):
+        for i in range(conv_w):
+            conv[img, j, i] = (np.sum(images[img,
                                j*sh:(kh+(j*sh)),
                                i*sw:(kw+(i*sw))] *
                                kernel, axis=(1, 2)))
