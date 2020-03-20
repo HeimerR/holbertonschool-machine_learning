39289b27042420f73e61e2b6981f5f550721da8b
heimerr <732@holbertonschool.com>
2020-03-20 03:08:56 +0000

    tuple condition fixed

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -1,49 +1,58 @@
 #!/usr/bin/env python3
-"""Convolutional Neural Networks"""
+""" Convolution with Padding """
 import numpy as np
 
 
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
-    """performs a same convolurion on grayscale image
-    Arg:
-       images: np.array containing grayscale img (m, h, w)
-       kernel: filter for the convolution (kh, kw)
-       padding: tuple of (ph, pw), ‘same’, or ‘valid’
-       stride: tuple (sh, sw)
-    Return: padded convolved images np.array
+    """ performs a same convolution on grayscale images:
+
+        @images is a numpy.ndarray with shape (m, h, w)
+            containing multiple grayscale images
+            @m is the number of images
+            @h is the height in pixels of the images
+            @w is the width in pixels of the images
+        @kernel is a numpy.ndarray with shape (kh, kw)
+            containing the kernel for the convolution
+        @kh is the height of the kernel
+        @kw is the width of the kernel
+        @padding is either a tuple of (ph, pw), ‘same’, or ‘valid’
+            if ‘same’, performs a same convolution
+            if ‘valid’, performs a valid convolution
+            if a tuple:
+                @ph is the padding for the height of the image
+                @pw is the padding for the width of the image
+        @stride is a tuple of (sh, sw)
+            @sh is the stride for the height of the image
+            @sw is the stride for the width of the image
+        Returns: a numpy.ndarray containing the convolved images
     """
     m = images.shape[0]
-    img_h = images.shape[1]
-    img_w = images.shape[2]
-    k_h = kernel.shape[0]
-    k_w = kernel.shape[1]
-    s_h, s_w = stride
-
+    h = images.shape[1]
+    w = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+    sh = stride[0]
+    sw = stride[1]
     if padding == 'valid':
-        p_h = 0
-        p_w = 0
-
+        ph = 0
+        pw = 0
     if padding == 'same':
-        p_h = int(((s_h*img_h) - s_h + k_h - img_h) / 2) + 1
-        p_w = int(((s_w*img_w) - s_w + k_w - img_w) / 2) + 1
-
+        ph = int(((h-1)*sh+kh-h)/2) + 1
+        pw = int(((w-1)*sw+kw-w)/2) + 1
     if type(padding) == tuple:
-        p_h = padding[0]
-        p_w = padding[1]
-
-    images = np.pad(images, [(0, 0), (p_h, p_h), (p_w, p_w)],
-                    mode='constant', constant_values=0)
-
-    out_h = int(((img_h - k_h + (2*p_h)) / (stride[0])) + 1)
-    out_w = int(((img_w - k_w + (2*p_w)) / (stride[1])) + 1)
-    output_conv = np.zeros((m, out_h, out_w))
-    m_img = np.arange(0, m)
-
-    for i in range(out_h):
-        for j in range(out_w):
-            output_conv[m_img, i, j] = np.sum(np.multiply(
-                images[m_img,
-                       i*(stride[0]):k_h+(i*(stride[0])),
-                       j*(stride[1]):k_w+(j*(stride[1]))],
-                kernel), axis=(1, 2))
-    return output_conv
+        ph = padding[0]
+        pw = padding[1]
+    if padding == 'same' or type(padding) == tuple:
+        images = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
+                        mode='constant', constant_values=0)
+    conv_h = int(((h+2*ph-kh)/sh) + 1)
+    conv_w = int(((w+2*pw-kw)/sw) + 1)
+    conv = np.zeros((m, conv_h, conv_w))
+    img = np.arange(m)
+    for j in range(conv_h):
+        for i in range(conv_w):
+            conv[img, j, i] = (np.sum(images[img,
+                               j*sh:(kh+(j*sh)),
+                               i*sw:(kw+(i*sw))] *
+                               kernel, axis=(1, 2)))
+    return conv
