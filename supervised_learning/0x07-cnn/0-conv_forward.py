b5363c647e43e8ec7b327d83b5d72d323894e800
heimerr <732@holbertonschool.com>
2020-03-25 02:14:05 +0000

    new odd even conditions

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -39,15 +39,25 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     sw = stride[1]
     ph = 0
     pw = 0
+    conv_h = int(((h_prev+2*ph-kh)/sh) + 1)
+    conv_w = int(((w_prev+2*pw-kw)/sw) + 1)
     if padding == 'same':
-        ph = int(((h_prev)*sh+kh-h_prev)/2)
-        pw = int(((w_prev)*sw+kw-w_prev)/2)
+        if kh % 2 == 0:
+            ph = int(((h_prev)*sh+kh-h_prev)/2)
+            conv_h = int(((h_prev+2*ph-kh)/sh))
+        else:
+            ph = int(((h_prev-1)*sh+kh-h_prev)/2)
+            conv_h = int(((h_prev+2*ph-kh)/sh)+1)
+        if kw % 2 == 0:
+            pw = int(((w_prev)*sw+kw-w_prev)/2)
+            conv_w = int(((w_prev+2*pw-kw)/sw))
+        else:
+            pw = int(((w_prev-1)*sw+kw-w_prev)/2)
+            conv_w = int(((w_prev+2*pw-kw)/sw)+1)
         A_prev = np.pad(A_prev, pad_width=((0, 0),
                         (ph, ph), (pw, pw), (0, 0)),
                         mode='constant', constant_values=0)
 
-    conv_h = int(((h_prev+2*ph-kh)/sh) + 1)
-    conv_w = int(((w_prev+2*pw-kw)/sw) + 1)
     conv = np.zeros((m, conv_h, conv_w, c_new))
     img = np.arange(m)
     for j in range(conv_h):
