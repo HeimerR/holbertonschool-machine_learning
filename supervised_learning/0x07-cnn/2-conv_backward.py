5ffccb05a6aa1ebab2f9fd9eba29074ed1b35a0e
heimerr <732@holbertonschool.com>
2020-04-03 01:45:23 +0000

    version based on male

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -1,58 +1,71 @@
 #!/usr/bin/env python3
-"""Convolutional Neural Networks"""
+""" Convolutional Back Prop """
 import numpy as np
 
 
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
-    """back prop convolutional 3D image, RGB image - color
-    Arg:
-       dZ: containing the partial derivatives (m, h_new, w_new, c_new)
-       A_prev: contains the output of prev layer (m, h_prev, w_prev, c_prev)
-       W: filter for the convolution (kh, kw, c_prev, c_new)
-       b: biases (1, 1, 1, c_new)
-       padding: string ‘same’, or ‘valid’
-       stride: tuple (sh, sw)
-    Returns: parcial dev prev layer (dA_prev), kernels (dW), biases (db)
     """
-    k_h, k_w, c_prev, c_new = W.shape
-    _, h_new, w_new, c_new = dZ.shape
-    m, h_x, w_x, c_prev = A_prev.shape
-    s_h, s_w = stride
-    x = A_prev
+    performs back propagation over a convolutional layer of a neural network:
 
-    if padding == 'valid':
-        p_h = 0
-        p_w = 0
-
-    if padding == 'same':
-        p_h = np.ceil(((s_h*h_x) - s_h + k_h - h_x) / 2)
-        p_h = int(p_h)
-        p_w = np.ceil(((s_w*w_x) - s_w + k_w - w_x) / 2)
-        p_w = int(p_w)
+    @dZ numpy.ndarray of shape (m, h_new, w_new, c_new) containing the
+        partial derivatives with respect to the unactivated output
+        of the convolutional layer
+        @m is the number of examples
+        @h_new is the height of the output
+        @w_new is the width of the output
+        @c_new is the number of channels in the output
+    @A_prev numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+        containing the output of the previous layer
+        @h_prev is the height of the previous layer
+        @w_prev is the width of the previous layer
+        @c_prev is the number of channels in the previous layer
+    @W is a numpy.ndarray of shape (kh, kw, c_prev, c_new) containing
+        the kernels for the convolution
+        @kh is the filter height
+        @kw is the filter width
+    @b is a numpy.ndarray of shape (1, 1, 1, c_new) containing the biases
+        applied to the convolution
+    @padding is a string that is either same or valid,
+        indicating the type of padding used
+    @stride is a tuple of (sh, sw) containing the strides for the convolution
+        @sh is the stride for the height
+        @sw is the stride for the width
+    Returns: the partial derivatives with respect to the
+        previous layer (dA_prev), the kernels (dW),
+        and the biases (db), respectively
+    """
+    m, h_new, w_new, c_new = dZ.shape
+    _, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, _, _ = W.shape
+    sh, sw = stride
+    ph = 0
+    pw = 0
 
     db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
 
-    x_padded = np.pad(x, [(0, 0), (p_h, p_h), (p_w, p_w), (0, 0)],
-                      mode='constant', constant_values=0)
+    if padding == 'same':
+        ph = np.ceil(((sh*h_prev)-sh+kh-h_prev)/2)
+        ph = int(ph)
+        pw = np.ceil(((sw*w_prev)-sw+kw-w_prev)/2)
+        pw = int(pw)
+
+    A_prev = np.pad(A_prev, pad_width=((0, 0),
+                    (ph, ph), (pw, pw), (0, 0)),
+                    mode='constant', constant_values=0)
 
     dW = np.zeros_like(W)
-    dx = np.zeros(x_padded.shape)
-    m_i = np.arange(m)
-    for i in range(m):
+    dx = np.zeros_like(A_prev)
+    for m_i in range(m):
         for h in range(h_new):
             for w in range(w_new):
                 for f in range(c_new):
-                    dx[i,
-                       h*(stride[0]):(h*(stride[0]))+k_h,
-                       w*(stride[1]):(w*(stride[1]))+k_w,
-                       :] += dZ[i, h, w, f] * W[:, :, :, f]
+                    tmp_W = W[:, :, :, f]
+                    tmp_dz = dZ[m_i, h, w, f]
+                    dx[m_i, h*sh:h*sh+kh, w*sw:w*sw+kw, :] += tmp_dz * tmp_W
 
-                    dW[:, :,
-                       :, f] += x_padded[i,
-                                         h*(stride[0]):(h*(stride[0]))+k_h,
-                                         w*(stride[1]):(w*(stride[1]))+k_w,
-                                         :] * dZ[i, h, w, f]
+                    tmp_A_prev = A_prev[m_i, h*sh:h*sh+kh, w*sw:w*sw+kw, :]
+                    dW[:, :, :, f] += tmp_A_prev * tmp_dz
 
-    dx = dx[:, p_h:dx.shape[1]-p_h, p_w:dx.shape[2]-p_w, :]
+    dx = dx[:, ph:dx.shape[1]-ph, pw:dx.shape[2]-pw, :]
 
     return dx, dW, db
