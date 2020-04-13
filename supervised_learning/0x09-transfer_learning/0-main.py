80efaa46bdf19864ebcbfeff1c2cafe3b4a05954
heimerr <ing.heimer.rojas@gmail.com>
2020-04-13 18:58:17 +0000

    88 percent version densenet

--- supervised_learning/0x09-transfer_learning/0-main.py
+++ supervised_learning/0x09-transfer_learning/0-main.py
@@ -9,5 +9,5 @@ K.learning_phase = K.backend.learning_phase
 _, (X, Y) = K.datasets.cifar10.load_data()
 X_p, Y_p = preprocess_data(X, Y)
 model = K.models.load_model('cifar10.h5')
-results = model.evaluate(X_p, Y_p)
+results = model.evaluate(X_p, Y_p, batch_size=128, verbose=1)
 print('test loss, test acc:', results)
--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -8,9 +8,11 @@
     Script doesn't run when the file is imported
 """
 import tensorflow.keras as K
+import numpy as np
 
 
 if __name__ == '__main__':
+    """ trains the model and save it """
     input_tensor = K.Input(shape=(32, 32, 3))
     (x_train, y_train), (x_test, y_test) = K.datasets.cifar10.load_data()
     x_train = K.applications.densenet.preprocess_input(x_train)
@@ -18,29 +20,54 @@ if __name__ == '__main__':
     x_test = K.applications.densenet.preprocess_input(x_test)
     y_test = K.utils.to_categorical(y_test, 10)
 
-    model = K.applications.densenet.DenseNet121(include_top=False, pooling='avg', input_tensor=input_tensor, weights='imagenet')
-    for layer in model.layers:
-	    layer.trainable = False
+    x_train = np.concatenate((x_train, np.flip(x_train, 2)), 0)
+    y_train = np.concatenate((y_train, y_train), 0)
+
+    model = K.applications.densenet.DenseNet121(include_top=False,
+                                                pooling='max',
+                                                input_tensor=input_tensor,
+                                                weights='imagenet')
+
     output = model.layers[-1].output
-    #output = K.layers.Flatten()(output)
     output = K.layers.Dense(512, activation='relu')(output)
+    output = K.layers.Dropout(0.3)(output)
+    output = K.layers.Dense(128, activation='relu')(output)
+    output = K.layers.Dropout(0.3)(output)
     output = K.layers.Dense(10, activation='softmax')(output)
+
     model = K.models.Model(inputs=model.inputs, outputs=output)
-    model.summary()
+
+    lrr = K.callbacks.ReduceLROnPlateau(
+                                       monitor='val_acc',
+                                       factor=.01,
+                                       patience=3,
+                                       min_lr=1e-5)
+
+    es = K.callbacks.EarlyStopping(monitor='val_acc',
+                                   mode='max',
+                                   verbose=1,
+                                   patience=10)
+
+    mc = K.callbacks.ModelCheckpoint('cifar10.h5',
+                                     monitor='val_acc',
+                                     mode='max',
+                                     verbose=1,
+                                     save_best_only=True)
+
     model.compile(optimizer='adam',
-                    loss='categorical_crossentropy',
-                    metrics=['acc'])
+                  loss='categorical_crossentropy',
+                  metrics=['acc'])
+
     history = model.fit(x_train, y_train,
-                    validation_data=(x_test, y_test),
-                    batch_size=64,
-                    epochs=5,
-                    verbose=1)
-    """
-    history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=100,
-                              validation_data=val_generator, validation_steps=50,
-                              verbose=1)
-    """
+                        validation_data=(x_test, y_test),
+                        batch_size=128,
+                        callbacks=[es, mc, lrr],
+                        epochs=30,
+                        verbose=1)
+
     model.save('cifar10.h5')
+
+
 def preprocess_data(X, Y):
     """ pre-processes the data for your model
 
