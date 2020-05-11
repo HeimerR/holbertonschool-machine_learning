6f978bcba9bc8888496e5290f147cf3b0ec3088d
heimerr <ing.heimer.rojas@gmail.com>
2020-05-11 19:41:20 +0000

    mean format fixed

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -23,7 +23,10 @@ def mean_cov(X):
         raise TypeError("X must be a 2D numpy.ndarray")
     if X.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
-    mean = np.mean(X, axis=0)
-    deviation = np.add(X, -mean)
-    cov = np.matmul(X.T, deviation) / X.shape[0]
+    n = X.shape[0]
+    d = X.shape[1]
+    mean = np.mean(X, axis=0).reshape(1, d)
+    deviation = np.tile(mean, n).reshape(n, d)
+    cov = X - deviation
+    cov = np.matmul(cov.T, cov)/(n - 1)
     return mean, cov
