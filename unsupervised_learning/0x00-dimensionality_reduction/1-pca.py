cb865b8d3ce9791233a33d1ff492ba2ef532c551
heimerr <ing.heimer.rojas@gmail.com>
2020-05-14 20:55:29 +0000

    rollback negative version

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -15,7 +15,17 @@ def pca(X, ndim):
     the transformed version of X
 
     """
+    """
     X_m = X - np.mean(X, axis=0)
     u, s, vh = np.linalg.svd(X_m)
     T = np.matmul(u[:, :ndim], np.diag(s[:ndim]))
-    return -T
+
+    """
+    X_m = X - np.mean(X, axis=0)
+
+    u, s, vh = np.linalg.svd(X_m)
+    W = vh[:ndim].T
+
+    T = np.matmul(X_m, (W))
+
+    return T
