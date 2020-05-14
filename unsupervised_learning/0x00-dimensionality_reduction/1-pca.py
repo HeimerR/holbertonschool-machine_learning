0b3158e79ea7f46f3eca7922b5e3dd2258a9321e
heimerr <ing.heimer.rojas@gmail.com>
2020-05-14 03:40:59 +0000

    basic t-sne added

---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" PCA """
+import numpy as np
+
+
+def pca(X, ndim):
+    """ performs PCA on a dataset:
+
+    - X is a numpy.ndarray of shape (n, d) where:
+        - n is the number of data points
+        - d is the number of dimensions in each point
+
+    - ndim is the new dimensionality of the transformed X
+    Returns: T, a numpy.ndarray of shape (n, ndim) containing
+    the transformed version of X
+
+    """
+    X_m = X - np.mean(X, axis=0)
+    u, s, vh = np.linalg.svd(X_m)
+    T = np.matmul(u[:, :ndim], np.diag(s[:ndim]))
+    return T
