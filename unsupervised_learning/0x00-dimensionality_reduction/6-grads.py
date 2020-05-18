3bdb1ff3a0f96a6baa725d4403793eeba712d2b6
heimerr <ing.heimer.rojas@gmail.com>
2020-05-18 02:43:57 +0000

    tile var PQ use outside

--- unsupervised_learning/0x00-dimensionality_reduction/6-grads.py
+++ unsupervised_learning/0x00-dimensionality_reduction/6-grads.py
@@ -8,10 +8,8 @@ def grads(Y, P):
     """  calculates the gradients of Y
         - Y is a numpy.ndarray of shape (n, ndim) containing
         the low dimensional transformation of X
-
         - P is a numpy.ndarray of shape (n, n) containing
         the P affinities of X
-
         Returns: (dY, Q)
             - dY is a numpy.ndarray of shape (n, ndim) containing
             the gradients of Y
@@ -21,7 +19,8 @@ def grads(Y, P):
     Q, num = Q_affinities(Y)
     n, m = Y.shape
     dY = np.zeros((n, m))
+    PQ = P-Q
     for i in range(n):
-        dY[i, :] = (np.sum(np.tile((P-Q)[:, i] * num[:, i],
+        dY[i, :] = (np.sum(np.tile(PQ[:, i] * num[:, i],
                     (m, 1)).T * (Y[i, :] - Y), 0))
-    return dY, Q
+    return (dY, Q)
