fdbae950d81358d71941fbcaf2375a583b32ce5e
heimerr <ing.heimer.rojas@gmail.com>
2020-05-20 23:48:03 +0000

    comments delted test

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -7,27 +7,9 @@ def kmeans(X, k, iterations=1000):
     """
         performs K-means on a dataset:
 
-        - X is a numpy.ndarray of shape (n, d) containing the dataset
-            - n is the number of data points
-            - d is the number of dimensions for each data point
-        - k is a positive integer containing the number of clusters
-
-        iterations is a positive integer containing the maximum number
-            of iterations that should be performed
-        If no change occurs between iterations, your function should return
-        Initialize the cluster centroids using a multivariate uniform
-            distribution
-        If a cluster contains no data points during the update step,
-            reinitialize its centroid
-
-        Returns: C, clss, or None, None on failure
-        - C is a numpy.ndarray of shape (k, d) containing the centroid means
-            for each cluster
-        - clss is a numpy.ndarray of shape (n,) containing the index of the
-            cluster in C that each data point belongs to
     """
 
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2 or X.shape[1] < 1:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
     if type(k) != int or k <= 0 or X.shape[0] <= k:
         return None, None
