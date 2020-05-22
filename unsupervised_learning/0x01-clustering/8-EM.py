0009a50f1bf452ccfbdc3f46ea50e9e84cf3f9a3
heimerr <ing.heimer.rojas@gmail.com>
2020-05-22 16:54:08 +0000

    test shorter code

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -9,6 +9,31 @@ maximization = __import__('7-maximization').maximization
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """ performs the expectation maximization for a GMM:
 
+        - X is a numpy.ndarray of shape (n, d) containing the data set
+        - k is a positive integer containing the number of clusters
+        - iterations is a positive integer containing the maximum number of
+            iterations for the algorithm
+        - tol is a non-negative float containing tolerance of the log
+            likelihood, used to determine early stopping i.e. if the
+            difference is less than or equal to tol you should stop the
+            algorithm
+        - verbose is a boolean that determines if you should print information
+            about the algorithm
+            If True, print "Log Likelihood after {i} iterations: {l}" every 10
+            iterations and after the last iteration
+            {i} is the number of iterations of the EM algorithm
+            {l} is the log likelihood
+
+        Returns: pi, m, S, g, l, or None, None, None, None, None on failure
+            - pi is a numpy.ndarray of shape (k,) containing the priors for
+                each cluster
+            - m is a numpy.ndarray of shape (k, d) containing the centroid
+                means for each cluster
+            - S is a numpy.ndarray of shape (k, d, d) containing the
+                covariance matrices for each cluster
+            - g is a numpy.ndarray of shape (k, n) containing the probabilities
+                for each data point in each cluster
+            - l is the log likelihood of the model
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None, None
@@ -20,7 +45,7 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
         return None, None, None, None, None
     if type(verbose) != bool:
         return None, None, None, None, None
-
+    """
     pi, m, S = initialize(X, k)
     n, d = X.shape
     l_prev = 0
@@ -35,3 +60,29 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
         l_prev = li
 
     return pi, m, S, g, li
+    """
+    pi, m, S = initialize(X, k)
+    l_past = 0
+
+    for i in range(iterations):
+        g, log_l = expectation(X, pi, m, S)
+        pi, m, S = maximization(X, g)
+
+        if (verbose is True):
+            if (i % 10 == 0) or (i == 0):
+                print("Log Likelihood after {} iterations: {}".format(
+                    i, log_l))
+            if (i == iterations - 1):
+                print("Log Likelihood after {} iterations: {}".format(
+                    i, log_l))
+            if abs(log_l - l_past) <= tol:
+                print("Log Likelihood after {} iterations: {}".format(
+                    i, log_l))
+                break
+
+        if abs(log_l - l_past) <= tol:
+            break
+
+        l_past = log_l
+
+    return (pi, m, S, g, log_l)
