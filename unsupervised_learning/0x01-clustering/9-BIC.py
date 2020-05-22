fc61e2534a500fe80397c93250b2f68bd4e79b07
heimerr <ing.heimer.rojas@gmail.com>
2020-05-22 17:20:52 +0000

    try except added

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -39,38 +39,40 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
                 - n is the number of data points used to create the model
                 - l is the log likelihood of the model
     """
+    try:
+        if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+            return None, None, None, None
+        if type(kmin) != int or kmin <= 0 or X.shape[0] <= kmin:
+            return None, None, None, None
+        if type(kmax) != int or kmax <= 0 or X.shape[0] <= kmax:
+            return None, None, None, None
+        if type(iterations) != int or iterations <= 0:
+            return None, None, None, None
+        if type(tol) != float or tol < 0:
+            return None, None, None, None
+        if type(verbose) != bool:
+            return None, None, None, None
 
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        n, d = X.shape
+        l_t = np.empty((kmax-kmin+1, ))
+        b = np.empty((kmax-kmin+1, ))
+        pi_t = []
+        m_t = []
+        S_t = []
+        for k in range(kmin, kmax+1):
+            pi, m, S, g, li = expectation_maximization(X,
+                                                       k,
+                                                       iterations,
+                                                       tol,
+                                                       verbose)
+            pi_t.append(pi)
+            m_t.append(m)
+            S_t.append(S)
+            l_t[k-1] = li
+            p = (k * d * (d+1) / 2) + (d * k) + k - 1
+            b[k-1] = p*np.log(n) - 2*li
+        best_k = np.argmin(b)
+        best_result = (pi_t[best_k], m_t[best_k], S_t[best_k])
+        return best_k+1, best_result, l_t, b
+    except Exception:
         return None, None, None, None
-    if type(kmin) != int or kmin <= 0 or X.shape[0] <= kmin:
-        return None, None, None, None
-    if type(kmax) != int or kmax <= 0 or X.shape[0] <= kmax:
-        return None, None, None, None
-    if type(iterations) != int or iterations <= 0:
-        return None, None, None, None
-    if type(tol) != float or tol < 0:
-        return None, None, None, None
-    if type(verbose) != bool:
-        return None, None, None, None
-
-    n, d = X.shape
-    l_t = np.empty((kmax-kmin+1, ))
-    b = np.empty((kmax-kmin+1, ))
-    pi_t = []
-    m_t = []
-    S_t = []
-    for k in range(kmin, kmax+1):
-        pi, m, S, g, li = expectation_maximization(X,
-                                                   k,
-                                                   iterations,
-                                                   tol,
-                                                   verbose)
-        pi_t.append(pi)
-        m_t.append(m)
-        S_t.append(S)
-        l_t[k-1] = li
-        p = (k * d * (d+1) / 2) + (d * k) + k - 1
-        b[k-1] = p*np.log(n) - 2*li
-    best_k = np.argmin(b)
-    best_result = (pi_t[best_k], m_t[best_k], S_t[best_k])
-    return best_k+1, best_result, l_t, b
