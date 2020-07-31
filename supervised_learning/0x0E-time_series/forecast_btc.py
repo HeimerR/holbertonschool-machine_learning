ff19a21402528ed1163794d2ae9b2e6f31dc2ca1
heimerr <ing.heimer.rojas@gmail.com>
2020-07-31 21:45:39 +0000

    bitcoin forecast added

--- supervised_learning/0x0E-time_series/forecast_btc.py
+++ supervised_learning/0x0E-time_series/forecast_btc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Time Series Forecasting - preprocess"""
+""" Time Series Forecasting for bitcoin"""
 import tensorflow as tf
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -8,25 +8,106 @@ import os
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-mpl.rcParams['figure.figsize'] = (8, 6)
-mpl.rcParams['axes.grid'] = False
-f = './coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv'
-df = pd.read_csv(f)
-days = 730
-df = df.iloc[-days*24*60:]
-df = df.drop(['Volume_(Currency)'], axis=1)
-df['Datetime'] = pd.to_datetime(df['Timestamp'], unit='s')
-df = df.drop(['Timestamp'], axis=1)
-df = df.set_index('Datetime')
-df = df.interpolate()
-df.columns = df.columns.str.replace('(', '')
-df.columns = df.columns.str.replace(')', '')
-df_mul = pd.DataFrame()
-df_mul['High'] = df.High.resample('H').max()
-df_mul['Low'] = df.Low.resample('H').min()
-df_mul['Weighted_Price'] = df.Weighted_Price.resample('H').mean()
-df_mul['Volume_BTC'] = df.Volume_BTC.resample('H').sum()
-df_W = df.Weighted_Price.resample('H').mean()
+def multivariate_data(dataset, target, start_index, end_index, history_size,
+                      target_size, step, single_step=False):
+    """ creates slide windows in an array """
+    data = []
+    labels = []
 
-df_W.plot(subplots=True)
-df = df_W.values
+    start_index = start_index + history_size
+    if end_index is None:
+    end_index = len(dataset) - target_size
+
+    for i in range(start_index, end_index):
+    indices = range(i-history_size, i, step)
+    data.append(dataset[indices])
+
+    if single_step:
+        labels.append(target[i+target_size])
+    else:
+        labels.append(target[i:i+target_size])
+
+    return np.array(data), np.array(labels)
+
+
+
+
+
+def univariate_data(dataset, start_index, end_index, history_size, target_size):
+    """ creates slide windows in an array """
+    data = []
+    labels = []
+
+    start_index = start_index + history_size
+    if end_index is None:
+    end_index = len(dataset) - target_size
+
+    for i in range(start_index, end_index):
+    indices = range(i-history_size, i)
+    data.append(np.reshape(dataset[indices], (history_size, 1)))
+    labels.append(dataset[i+target_size])
+
+    return np.array(data), np.array(labels)
+
+
+def univariate():
+
+    univariate_past_history = 24
+    univariate_future_target = 0
+
+    x_train_uni, y_train_uni = univariate_data(uni_data, 0, TRAIN_SPLIT,
+                                               univariate_past_history,
+                                               univariate_future_target)
+    x_val_uni, y_val_uni = univariate_data(uni_data, TRAIN_SPLIT, None,
+                                           univariate_past_history,
+                                           univariate_future_target)
+    print ('Single window of past history')
+    print(x_train_uni.shape)
+    print (x_train_uni[0])
+    print (x_train_uni[1])
+    print ('\n Target price to predict')
+    print (y_train_uni[0])
+
+
+def multivariate():
+
+    df_mul.plot(subplots=True)
+    dataset = df_mul.values
+    data_mean = dataset[:TRAIN_SPLIT].mean(axis=0)
+    data_std = dataset[:TRAIN_SPLIT].std(axis=0)
+    dataset = (dataset-data_mean)/data_std
+    past_history = 24
+    future_target = 0
+    STEP = 1
+
+    x_train_single, y_train_single = multivariate_data(dataset, dataset[:, 2], 0,
+                                                       TRAIN_SPLIT, past_history,
+                                                       future_target, STEP,
+                                                       single_step=True)
+    x_val_single, y_val_single = multivariate_data(dataset, dataset[:, 2],
+                                                   TRAIN_SPLIT, None, past_history,
+                                               future_target, STEP,
+                                               single_step=True)
+
+if __name__ == '__main__':
+
+    preprocess = __import__('preprocess_data').preprocessor
+
+    df_W df_mul, = preprocess('./coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', 730)
+    df = df_W.values
+
+    TRAIN_SPLIT = int(days*0.8*24) # length of training dataset
+
+    tf.random.set_seed(13)
+
+    uni_train_mean = df[:TRAIN_SPLIT].mean()
+    uni_train_std = df[:TRAIN_SPLIT].std()
+    uni_data = (df-uni_train_mean)/uni_train_std
+
+    # plots training and test dataset
+
+    plt.figure(figsize=(14,4))
+    plt.plot(df_W.index[:TRAIN_SPLIT], df[:TRAIN_SPLIT])
+    plt.plot(df_W.index[TRAIN_SPLIT:], df[TRAIN_SPLIT:])
+    plt.legend(["Training", "Test"])
+    plt.grid()
--- supervised_learning/0x0E-time_series/preprocess_data.py
+++ supervised_learning/0x0E-time_series/preprocess_data.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python3
 """ Time Series Forecasting - preprocess"""
-import tensorflow as tf
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 import numpy as np
-import os
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 
-mpl.rcParams['figure.figsize'] = (8, 6)
-mpl.rcParams['axes.grid'] = False
-f = './coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv'
-df = pd.read_csv(f)
-days = 730
-df = df.iloc[-days*24*60:]
-df = df.drop(['Volume_(Currency)'], axis=1)
-df['Datetime'] = pd.to_datetime(df['Timestamp'], unit='s')
-df = df.drop(['Timestamp'], axis=1)
-df = df.set_index('Datetime')
-df = df.interpolate()
-df.columns = df.columns.str.replace('(', '')
-df.columns = df.columns.str.replace(')', '')
-df_mul = pd.DataFrame()
-df_mul['High'] = df.High.resample('H').max()
-df_mul['Low'] = df.Low.resample('H').min()
-df_mul['Weighted_Price'] = df.Weighted_Price.resample('H').mean()
-df_mul['Volume_BTC'] = df.Volume_BTC.resample('H').sum()
-df_W = df.Weighted_Price.resample('H').mean()
+def preprocessor(datafile, days):
+    """ pre-process dataset:
+        fills empty values, re-sample the time series
+        and split the data
+        - datafile: string dataset location
+        - days: days to take from the dataset (the last)
+        returns:
+            df_W is the data for univarite
+            df_mul is the data for multivariate
+    """
+    df = pd.read_csv(datafile)
+    df = df.iloc[-days*24*60:]
+    df = df.drop(['Volume_(Currency)'], axis=1) # volume dollar deleted
+    df['Datetime'] = pd.to_datetime(df['Timestamp'], unit='s')
+    df = df.drop(['Timestamp'], axis=1)
+    df = df.set_index('Datetime')
+    df = df.interpolate() # completes null spots
+    df.columns = df.columns.str.replace('(', '')
+    df.columns = df.columns.str.replace(')', '')
+    df_mul = pd.DataFrame() # multivariate
+    df_mul['High'] = df.High.resample('H').max()
+    df_mul['Low'] = df.Low.resample('H').min()
+    df_mul['Weighted_Price'] = df.Weighted_Price.resample('H').mean()
+    df_mul['Volume_BTC'] = df.Volume_BTC.resample('H').sum()
+    df_W = df.Weighted_Price.resample('H').mean() # univariate
 
-df_W.plot(subplots=True)
-df = df_W.values
+    df_W.plot(subplots=True)
+
+    return df_W, df_mul
