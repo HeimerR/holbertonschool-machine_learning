ff7156cd5c327e2e3863b2668e6d087fbcfabc18
heimerr <ing.heimer.rojas@gmail.com>
2020-08-15 02:37:56 +0000

    testing deep n

--- supervised_learning/0x01-multiclass_classification/4-deep_neural_network.py
+++ supervised_learning/0x01-multiclass_classification/4-deep_neural_network.py
@@ -1,92 +1,108 @@
 #!/usr/bin/env python3
-""" DeepNeuralNetwork """
+""" Deep neural network """
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
 
-class DeepNeuralNetwork:
-    """ class DeepNeuralNetwork """
+class DeepNeuralNetwork():
+    """ defines a deep neural network """
     def __init__(self, nx, layers, activation='sig'):
-        """ init DeepNeuralNetwork """
-        if type(nx) != int:
-            raise TypeError('nx must be an integer')
+        """ Class constructor
+            @nx: number of input features
+            @layers: number of nodes in each layer of the network
+            @L: number of layers in the neural network
+            @cache: dictionary to hold all intermediary values of the network
+            @weights: dictionary to hold all weights and biased of the network
+            @activation: represents the type of activation function used in
+                         the hidden layers
+                sig represents a sigmoid activation
+                tanh represents a tanh activation
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError('nx must be a positive integer')
-        if type(layers) != list:
-            raise TypeError('layers must be a list of positive integers')
-        if len(layers) == 0:
-            raise TypeError('layers must be a list of positive integers')
-        if activation != 'sig' and activation != 'tanh':
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or not layers:
+            raise TypeError("layers must be a list of positive integers")
+        if activation != "sig" and activation != "tanh":
             raise ValueError("activation must be 'sig' or 'tanh'")
-        self.nx = nx
-        self.layers = layers
         self.__L = len(layers)
-        self.__activation = activation
         self.__cache = {}
         self.__weights = {}
-        for ly in range(self.L):
-            if type(layers[ly]) != int or layers[ly] <= 0:
-                raise TypeError('layers must be a list of positive integers')
-            self.weights["b"+str(ly+1)] = np.zeros((layers[ly], 1))
-            if ly == 0:
-                heetal = np.random.randn(layers[ly], nx) * np.sqrt(2/nx)
-                self.weights["W"+str(ly+1)] = heetal
+        self.__activation = activation
+
+        for i in range(self.L):
+            if type(layers[i]) is not int or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+
+            w = "W" + str(i + 1)
+            b = "b" + str(i + 1)
+
+            if i == 0:
+                self.weights[w] = np.random.randn(layers[i], nx)\
+                                  * np.sqrt(2. / nx)
             else:
-                factor = np.sqrt(2/layers[ly-1])
-                heetal = np.random.randn(layers[ly], layers[ly-1]) * factor
-                self.weights["W" + str(ly+1)] = heetal
+                self.weights[w] = np.random.randn(layers[i], layers[i - 1])\
+                                  * np.sqrt(2 / layers[i - 1])
+            self.weights[b] = np.zeros((layers[i], 1))
 
     @property
     def L(self):
-        """ The number of layers in the neural network """
+        """ Getter function """
         return self.__L
 
     @property
     def cache(self):
-        """ hold all intermediary values of the network """
+        """ Getter function """
         return self.__cache
 
     @property
     def weights(self):
-        """ hold all weights and biased of the network """
+        """ Getter function """
         return self.__weights
 
     @property
     def activation(self):
-        """ type of activation function used in the hidden layers """
+        """ Getter function """
         return self.__activation
 
     def forward_prop(self, X):
-        """ Calculates the forward propagation of the neural network """
-        self.__cache["A0"] = X
-        for ly in range(self.__L):
-            Zp = np.matmul(self.__weights["W"+str(ly+1)],
-                           self.__cache["A"+str(ly)])
-            Z = Zp + self.__weights["b"+str(ly+1)]
-            if ly == self.__L - 1:
-                t = np.exp(Z)
-                self.__cache["A"+str(ly+1)] = (t/np.sum(t, axis=0,
-                                               keepdims=True))
-            else:
-                if self.__activation == 'sig':
-                    self.__cache["A"+str(ly+1)] = 1/(1+np.exp(-Z))
-                else:
-                    self.__cache["A"+str(ly+1)] = np.tanh(Z)
-
-        return self.__cache["A"+str(self.__L)], self.__cache
+        """ Activation function
+            Sigmoid Forward propagation
+        """
+        self.__cache['A0'] = X
+        for i in range(self.L):
+            w = 'W' + str(i + 1)
+            b = 'b' + str(i + 1)
+            a = 'A' + str(i + 1)
+            Z = np.dot(self.__weights[w], self.__cache['A' + str(i)])\
+                + self.__weights[b]
+            if i + 1 == self.L:
+                exp = np.exp(Z)
+                self.__cache[a] = exp / np.sum(exp, axis=0, keepdims=True)
+            elif self.__activation == 'sig':
+                self.__cache[a] = 1 / (1 + np.exp(-Z))
+            elif self.__activation == 'tanh':
+                self.__cache[a] = np.tanh(Z)
+        return self.__cache[a], self.__cache
 
     def cost(self, Y, A):
-        """ Calculates the cost of the model using logistic regression """
-        C = np.sum(Y * np.log(A))
-        return (-1/(Y.shape[1])) * C
+        """ Calculates the cost of the model using logistic regression
+            @Y: one-hot numpy.ndarray of shape (classes, m)
+        """
+        m = Y.shape[1]
+        cost = -(1 / m) * np.sum(Y * np.log(A))
+        return cost
 
     def evaluate(self, X, Y):
-        """ Evaluates the neural network’s predictions """
+        """ Evaluates the neuron
+            @Y: one-hot numpy.ndarray of shape (classes, m)
+        """
         self.forward_prop(X)
-        tmp = np.amax(self.__cache["A"+str(self.__L)], axis=0)
-        return (np.where(self.__cache["A"+str(self.__L)] == tmp, 1, 0),
-                self.cost(Y, self.__cache["A"+str(self.__L)]))
+        i = 'A' + str(self.__L)
+        A = self.__cache[i]
+        return np.round(A).astype(int), self.cost(Y, A)
 
     def gradient_descent(self, Y, cache, alpha=0.05):
         """ Calculates one pass of gradient descent on the neural network """
@@ -115,56 +131,65 @@ class DeepNeuralNetwork:
             self.__weights["b"+str(ly+1)] = tmp_W["b"+str(ly+1)] - alpha * db
             dzp = dz
 
-    def train(self, X, Y, iterations=5000, alpha=0.05, verbose=True,
-              graph=True, step=100):
-        """ Trains the deep neural network """
-        if type(iterations) != int:
+    def train(self, X, Y, iterations=5000, alpha=0.05,
+              verbose=True, graph=True, step=100):
+        """ Trains the deep neural network
+            @X: Input data
+            @Y: Correct labels for the input data
+            @iterations: Number of iterations to train over
+            @alpha: Learning rate
+        """
+        if type(iterations) is not int:
             raise TypeError("iterations must be an integer")
-        if iterations <= 0:
+        if iterations < 1:
             raise ValueError("iterations must be a positive integer")
-        if type(alpha) != float:
+        if type(alpha) is not float:
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
+        if type(step) is not int:
+            raise TypeError("step must be an integer")
         if verbose is True or graph is True:
-            if type(step) != int:
-                raise TypeError("step must be an integer")
-            if step <= 0 or step > iterations:
+            if step > iterations:
                 raise ValueError("step must be positive and <= iterations")
-
-        steps = []
-        costs = []
+        it_x = []
+        cost_y = []
         for i in range(iterations + 1):
             self.forward_prop(X)
-            cost = self.cost(Y, self.__cache["A"+str(self.__L)])
-            if i % step == 0 or i == iterations:
-                costs.append(cost)
-                steps.append(i)
+            if (i % step) == 0:
+                a = 'A' + str(self.__L)
+                cost = self.cost(Y, self.__cache[a])
                 if verbose is True:
                     print("Cost after {} iterations: {}".format(i, cost))
+                if graph is True:
+                    it_x.append(i)
+                    cost_y.append(cost)
             if i < iterations:
                 self.gradient_descent(Y, self.__cache, alpha)
         if graph is True:
-            plt.plot(np.array(steps), np.array(costs))
-            plt.xlabel('iteration')
-            plt.ylabel('cost')
-            plt.suptitle("Training Cost")
-            plt.show()
+            plt.plot(it_x, cost_y)
+            plt.title("Training Cost")
+            plt.xlabel("iterations")
+            plt.ylabel("cost")
         return self.evaluate(X, Y)
 
     def save(self, filename):
         """ Saves the instance object to a file in pickle format """
-        if filename[-4:] != ".pkl":
-            filename = filename + ".pkl"
-        with open(filename, 'wb') as f:
-            pickle.dump(self, f)
+        if len(filename.split('.')) == 1:
+            filename += '.pkl'
+        try:
+            file_object = open(filename, 'wb')
+            pickle.dump(self, file_object)
+            file_object.close()
+        except Exception as e:
+            return None
 
-    @staticmethod
     def load(filename):
         """ Loads a pickled DeepNeuralNetwork object """
         try:
-            with open(filename, 'rb') as f:
-                obj = pickle.load(f)
-            return obj
-        except FileNotFoundError:
+            file_object = open(filename, 'rb')
+            a = pickle.load(file_object)
+            file_object.close()
+            return a
+        except Exception as e:
             return None
