1287628ca8d318afadbdd9de71ff1ad4ca6d0898
heimer rojas <ing.heimer.rojas@gmail.com>
2020-08-17 21:36:26 +0000

    cleaner version

--- reinforcement_learning/0x01-deep_q_learning/train.py
+++ reinforcement_learning/0x01-deep_q_learning/train.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import tensorflow
-print(tensorflow.__version__)
 import gym
 from rl.agents.dqn import DQNAgent
 from rl.policy import EpsGreedyQPolicy, LinearAnnealedPolicy
@@ -12,43 +10,56 @@ from rl.callbacks import ModelIntervalCheckpoint, FileLogger
 from PIL import Image
 import numpy as np
 
+
 class AtariProcessor(Processor):
+    """ preprocessing based on
+        Deep Learning Quick Reference by Mike Bernico """
     def process_observation(self, observation):
+        """ resizing and grayscale """
         assert observation.ndim == 3  # (height, width, channel)
         img = Image.fromarray(observation)
-        img = img.resize((84, 84), Image.ANTIALIAS).convert('L')  # resize and convert to grayscale
+        # resize and convert to grayscale
+        img = img.resize((84, 84), Image.ANTIALIAS).convert('L')
         processed_observation = np.array(img)
         assert processed_observation.shape == (84, 84)
-        return processed_observation.astype('uint8')  # saves storage in experience memory
+        # saves storage in experience memory
+        return processed_observation.astype('uint8')
 
     def process_state_batch(self, batch):
-        # We could perform this processing step in `process_observation`. In this case, however,
-        # we would need to store a `float32` array instead, which is 4x more memory intensive than
-        # an `uint8` array. This matters if we store 1M observations.
+        """ rescale
+        We could perform this processing step in `process_observation`.
+        In this case, however,
+        we would need to store a `float32` array instead, which is 4x
+        more memory intensive than
+        an `uint8` array. This matters if we store 1M observations.
+        """
         processed_batch = batch.astype('float32') / 255.
         return processed_batch
 
     def process_reward(self, reward):
+        """ rewards between -1 and 1 """
         return np.clip(reward, -1., 1.)
 
-"""
-def to_grayscale(img):
-    return np.mean(img, axis=2).astype(np.uint8)
 
-def downsample(img):
-    return img[::2, ::2]
-
-def preprocess(img):
-    return to_grayscale(downsample(img))
-"""
 def create_q_model(num_actions, window):
+    """ preprocessing based on
+        Deep Learning Quick Reference by Mike Bernico """
+
     # Network defined by the Deepmind paper
+
     inputs = layers.Input(shape=(window, 84, 84))
+    # comment the line below to use with GPU
+    inputs_sort = layers.Permute((2, 3, 1))(inputs)
 
     # Convolutions on the frames on the screen
-    layer1 = layers.Conv2D(32, 8, strides=4, activation="relu", data_format="channels_first")(inputs)
-    layer2 = layers.Conv2D(64, 4, strides=2, activation="relu", data_format="channels_first")(layer1)
-    layer3 = layers.Conv2D(64, 3, strides=1, activation="relu", data_format="channels_first")(layer2)
+    # Change data_format="channels_first" to use GPU
+    # change inputs_sort by inputs to use GPU
+    layer1 = layers.Conv2D(32, 8, strides=4, activation="relu",
+                           data_format="channels_last")(inputs_sort)
+    layer2 = layers.Conv2D(64, 4, strides=2, activation="relu",
+                           data_format="channels_last")(layer1)
+    layer3 = layers.Conv2D(64, 3, strides=1, activation="relu",
+                           data_format="channels_last")(layer2)
 
     layer4 = layers.Flatten()(layer3)
 
@@ -57,42 +68,35 @@ def create_q_model(num_actions, window):
 
     return K.Model(inputs=inputs, outputs=action)
 
-def build_callbacks(env_name):
-    checkpoint_weights_filename = 'dqn_' + env_name + '_weights_{step}.h5f'
-    log_filename = 'dqn_{}_log.json'.format(env_name)
-    callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=5000)]
-    callbacks += [FileLogger(log_filename, interval=100)]
-    return callbacks
 
 if __name__ == '__main__':
-    file_name = "policy"
     env = gym.make("Breakout-v0")
     env.reset()
     num_actions = env.action_space.n
-    print(num_actions)
     window = 4
     model = create_q_model(num_actions, window)
-    print(model.summary())
+    model.summary()
     memory = SequentialMemory(limit=1000000, window_length=window)
     processor = AtariProcessor()
-    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
-                                              nb_steps=1000000)
-    dqn = DQNAgent(model=model, nb_actions=num_actions, policy=policy, memory=memory, processor=processor,
-                   nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
+
+    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps',
+                                  value_max=1., value_min=.1, value_test=.05,
+                                  nb_steps=1000000)
+
+    dqn = DQNAgent(model=model, nb_actions=num_actions, policy=policy,
+                   memory=memory, processor=processor,
+                   nb_steps_warmup=50000, gamma=.99,
+                   target_model_update=10000,
                    train_interval=4,
                    delta_clip=1.)
-    dqn.compile(K.optimizers.Adam(lr=.00025), metrics=['mae'])
-    callbacks = build_callbacks(file_name)
 
+    dqn.compile(K.optimizers.Adam(lr=.00025), metrics=['mae'])
+    # training
     dqn.fit(env,
             nb_steps=17500,
             log_interval=10000,
             visualize=False,
-            verbose=2,
-            callbacks=callbacks)
+            verbose=2)
 
-    # After training is done, we save the final weights.
+    # save the final weights.
     dqn.save_weights('policy.h5', overwrite=True)
-
-    # Finally, evaluate our algorithm for 5 episodes.
-    dqn.test(env, nb_episodes=10, visualize=True)
