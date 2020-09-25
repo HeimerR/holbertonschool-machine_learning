87f738dd3df31d010e09c34aee731807c3ce20c0
heimer rojas <ing.heimer.rojas@gmail.com>
2020-09-25 03:20:31 +0000

    API tasks added

---
+++ pipeline/0x01-apis/0-passengers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Can I join? """
+import requests
+
+def availableShips(passengerCount):
+    """  returns the list of ships that can hold a
+         given number of passengers
+
+         If no ship available, return an empty list.
+    """
+
+    url = "https://swapi-api.hbtn.io/api/starships/"
+    ships = []
+    while url != None:
+        r = requests.get(url)
+        results = r.json()["results"]
+        for ship in results:
+            p = ship["passengers"]
+            p = p.replace(',', '')
+            if p.isnumeric() and int(p) >= passengerCount:
+                ships.append(ship["name"])
+        url = r.json()["next"]
+    return ships
