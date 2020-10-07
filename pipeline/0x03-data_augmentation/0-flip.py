6804902879771aa6b5adf148418e3454b9cb88f9
heimer rojas <ing.heimer.rojas@gmail.com>
2020-10-07 17:08:59 +0000

    data aug-flip

---
+++ pipeline/0x03-data_augmentation/0-flip.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Flip """
+import tensorflow as tf
+
+
+def flip_image(image):
+    """ flips an image horizontally:
+
+        image is a 3D tf.Tensor containing the image to flip
+
+        Returns the flipped image
+    """
+    flip = tf.image.flip_left_right(x)
+    return flip
+
