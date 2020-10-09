b27faafcf52ac2edd54df8cf19025e1cb1aad2ca
Heimer Rojas <ing.heimer.rojas@gmail.com>
2020-10-09 02:22:39 +0000

    shear for tf1.15

--- pipeline/0x03-data_augmentation/3-shear.py
+++ pipeline/0x03-data_augmentation/3-shear.py
@@ -13,4 +13,5 @@ def shear_image(image, intensity):
     """
     img = tf.keras.preprocessing.image.img_to_array(image)
     sheared_img = tf.keras.preprocessing.image.random_shear(img, intensity)
+    heared_img = tf.keras.preprocessing.image.array_to_img(sheared_img)
     return sheared_img
